87eb73d36622148f10d2430b11cb888b68a28e3c
saadgitt <saad.akbar@tintash.com>
2020-07-27 21:00:06 +0500

    Header: Added and updated header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import {HomePage} from './pages/homepage/homepage.component.jsx';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
+    
     <div>
+      <Header/>
       <Switch>
         <Route exact path= '/' component={HomePage}/>
         <Route path= '/shop' component={ShopPage}/>
