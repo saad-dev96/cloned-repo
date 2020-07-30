f9fcbf2057fe273403ed51a65dc9e82d43206c4e
Saadgitt <saadakbar119@gmail.com>
2020-07-30 13:30:55 +0500

    cardFeature: card adds and hides

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils'
 import {connect} from 'react-redux'
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className= 'header'>
         <Link className = 'logo-container' to="/">
             <Logo className = 'logo'></Logo>
@@ -17,12 +19,15 @@ const Header = ({currentUser}) => (
             currentUser ? (<div className = 'option' onClick={() => {auth.signOut()}}>Sign-out</div>)
             : (<Link className= 'option' to= '/signIn'>Sign-in</Link>)
             }
+            <CartIcon/>
         </div>
-
+        {hidden ? null : 
+        <CartDropdown/>}
     </div>
 );
-const mapStateToProps = state => ({
-    currentUser : state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 
